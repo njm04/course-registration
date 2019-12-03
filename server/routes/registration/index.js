5cdbdbc1a5db27db3f34bc526fb0fd6b90fde324
Neil John Medalla <medallaneiljohn@gmail.com>
2019-12-02 21:26:05 -0700

    refactored writeFile

--- server/routes/registration/index.js
+++ server/routes/registration/index.js
@@ -6,12 +6,13 @@ module.exports = (param) => {
 
     router.post('/', async(req, res, next) => {
         try {
-            const courseName = req.body.courseName.trim();
-            const courseCode = req.body.courseCode.trim();
-            const creditHour = req.body.creditHour.trim();
-            const term = req.body.term.trim();
+            const courseName = req.body.courseName;
+            const courseCode = req.body.courseCode;
+            const creditHour = req.body.creditHour;
+            const term = req.body.term;
             const timestamp = Date.now();
             const courseList = await courseRegistrationService.getData();
+            console.log(courseName, courseCode, creditHour, term)
             if(!courseName || !courseCode || !creditHour || !term) {
                 return res.render('courses', {
                     page: 'Courses',
@@ -25,12 +26,8 @@ module.exports = (param) => {
             } else {
                 await courseRegistrationService.registerCourse(courseName, courseCode, creditHour, term, timestamp);
                 // Updates the list when new data is added
-                const courseList = await courseRegistrationService.getData();
-                return res.render('courses', {
-                    page: 'Courses',
-                    success: true,
-                    courseList
-                });
+                // let courseList = await courseRegistrationService.getData();
+                res.redirect('/');
             }
         } catch(err) {
             return next(err);
@@ -40,7 +37,7 @@ module.exports = (param) => {
     router.get('/', async(req, res, next) => {
         try {
             const courseList = await courseRegistrationService.getData();
-            console.log("in get: ", courseList)
+            // return res.send(courseList)
             return res.render('courses', {
                 page: 'courses',
                 courseList
--- server/services/CourseRegistrationService.js
+++ server/services/CourseRegistrationService.js
@@ -4,7 +4,8 @@ const path = require("path");
 
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
-const dataPath = path.join(__dirname, './data');
+const appendFile = util.promisify(fs.appendFile);
+const dataPath = path.join(__dirname, './data/courses.json');
 
 class CourseRegistrationService {
     constructor(datafile) {
@@ -14,17 +15,21 @@ class CourseRegistrationService {
     async registerCourse(courseName, courseCode, creditHour, term, timestamp) {
         const data = await this.getData();
         data.push({courseName, courseCode, creditHour, term, "createdAt": timestamp});
+        if(!fs.existsSync(this.datafile)) return writeFile(this.datafile, JSON.stringify(data));
+        console.log('File already exists')
         return writeFile(this.datafile, JSON.stringify(data));
     }
 
     async getData() {
-        const data = await readFile(this.datafile, 'utf8');
-        if(!data) return [];
-        return JSON.parse(data);
+        if(fs.existsSync(this.datafile)) {
+            const data = await readFile(this.datafile, 'utf8');
+            if(!data) return [];
+            return JSON.parse(data);
+        } else {
+            return [];
+        }
+        
     }
-    // checkIfFileExist = () => {
-    //     if(fs.existsSync())
-    // }
 }
 
 module.exports = CourseRegistrationService;
